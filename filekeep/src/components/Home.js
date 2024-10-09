f43d93a3d5c8c4ca11239d26b308c9c5d847d868
sam-gupta-git <sam78748@gmail.com>
2024-10-09 04:23:36 -0500

    Minor change to new name

--- filekeep/src/components/Home.js
+++ filekeep/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to Filekeep</h1>
+      <h1>Welcome to FileRook</h1>
       {/* Add content for the home page */}
     </div>
   );
